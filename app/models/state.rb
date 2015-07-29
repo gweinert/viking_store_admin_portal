1a96dedd977bc66044c68c3f9ef538bc02047880
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-29 16:34:59 -0700

    show order page

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,9 +2,11 @@ class State < ActiveRecord::Base
   has_many :addresses
 
   def self.get_top_states
-    result = []
     table = User.joins("JOIN addresses ON users.billing_id = addresses.id")
-    top_states = table.select("addresses.state_id, count(*) AS users").group("addresses.state_id").order("users desc").limit(3)
+    top_states = table.select("addresses.state_id, count(*) AS users")
+                      .group("addresses.state_id")
+                      .order("users desc")
+                      .limit(3)
     table = top_states.joins("JOIN states ON state_id = states.id")
     table.select("states.name")
     result = [
@@ -13,5 +15,7 @@ class State < ActiveRecord::Base
           [table.select("states.name").third[:name], table.select("states.name").third[:users]]
         ]
   end
+
+  
   
 end
